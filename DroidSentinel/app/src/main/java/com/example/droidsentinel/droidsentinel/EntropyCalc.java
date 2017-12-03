3ded74ecbf17cc73935cbabfb956fe11d54e6562
voar <voar94@gmail.com>
2017-12-03 05:12:53 +0100

    info entropia

--- DroidSentinel/app/src/main/java/com/example/droidsentinel/droidsentinel/EntropyCalc.java
+++ DroidSentinel/app/src/main/java/com/example/droidsentinel/droidsentinel/EntropyCalc.java
@@ -3,7 +3,20 @@ package com.example.droidsentinel.droidsentinel;
 /**
  * Created by andreshg on 30/11/17.
  */
+/**
+ * Ventajas de entropia frente a otroas metricas:
+ I. Ozcelik, R.R. Brooks. “Deceiving entropy based DoS detection”, Computers & Security, vol. 48, no. 1, pp. 234-245, 2015.
 
+ Entropia de Shannon (información):
+ C.E. Shannon. “A mathematical theory of communication”. Bell Systems Technical Journal,  vol. 27,  pp. 379-656, 1948
+
+ Entropia de Rényi:
+ M.H. Bhuyan, D. K. Bhattacharyya, J.K. Kalita. “An empirical evaluation of information metrics for low-rate and high-rate DDoS attack detection”,
+ Pattern Recognition Letters, vol. 51, no. 1, pp. 1-7, 2015.
+
+ Hemos utilizado la Entropía de Shannon pese a haber implementado la Entropía de Renyi, pero por falta de tiempos
+ y pruebas nos hemos decantado por la Entropia de Shannon
+ */
 public class EntropyCalc {
 
     public static double LOG_BASE = 2.0;
